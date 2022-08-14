a92b55d9d8cc7bc37c2c7ae5e4b90e7a098bd2bb
Camille Herpin <camille.herpin@gmail.com>
2022-08-14 16:30:36 +0200

    Rate-limiter modifications

--- backend/middleware/rate-limit.js
+++ backend/middleware/rate-limit.js
@@ -1,8 +1,9 @@
 const rateLimit = require('express-rate-limit')
 
+//blocks requests based on IP address!
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000, //10 minutes
-    max: 2, //Limit each IP to 100 requests per `window` (here, per 10 minutes)
+    max: 5, //Limit each IP to 5 requests per `window` (here, per 10 minutes)
     message: 'Trop de tentatives de connexion, compte bloqué pour 10 minutes',
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
